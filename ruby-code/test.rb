9322d1318f026ca26d18de7ec454f1731af90a37
root <root@localhost.localdomain>
2021-06-04 03:46:21 -0400

    update gem

--- ruby-code/test.rb
+++ ruby-code/test.rb
@@ -5,12 +5,9 @@ require 'rspec/expectations'
 include RSpec::Matchers
 
 def setup
-  caps = Selenium::WebDriver::Remote::Capabilities.chrome
-  caps[:platform] = :linux # you can also use :any, :win, or :x
-
   @driver = Selenium::WebDriver.for(
     :remote,
-    url: 'http://172.20.0.2:4444/wd/hub',
+    url: 'http://10.8.90.248:4444/wd/hub',
     desired_capabilities: :chrome) # you can also use :chrome, :safari, etc.
 end
 
@@ -21,10 +18,10 @@ end
 def run
   setup
   yield
-  teardown
 end
 
 run do
   @driver.get 'https://google.com'
   expect(@driver.title).to eq('The Internet')
 end
+
